cd163ca4db7aabf1ccf77a17ce1e7a0fd686437a
maxwell hecht chaneski <cryptidmartyr@gmail.com>
2026-07-03 11:12:33 -0400

    Making a proffessional portfolio for my website

---
+++ Portfolio/index.js
@@ -0,0 +1,10 @@
+const button = document.querySelector("button");
+if (button) {
+  button.addEventListener("click", function() {
+    console.log("Button clicked!");
+  });
+} else {
+  console.error("Element not found!");
+}
+
+//pup
